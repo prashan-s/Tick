63200ab1506008157626ed144aabdebd9b621982
Prashan Samarathunge <prashan@bhasha.lk>
2024-03-15 08:11:58 +0530

    Minor Changes

---
+++ app/src/main/java/com/example/tick/DashboardActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tick
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.tick.R.layout.activity_dashboard)
+    }
+
+}
